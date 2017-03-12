c11af6fbf487a9c8fab2858c947b722b39bfea84
Peter Jang <peter.x.jang@gmail.com>
2017-03-12 09:29:57 -0500

    Add recipe new action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,10 @@ class RecipesController < ApplicationController
     render "index.html.erb"
   end
 
+  def new
+    render "new.html.erb"
+  end
+
   def show
     recipe_id = params[:id]
     @recipe = Recipe.find_by(id: recipe_id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "/" => "recipes#index"
   
   get "/recipes" => "recipes#index"
+  get "/recipes/new" => "recipes#new"
   get "/recipes/:id" => "recipes#show"
 end
