7a4087550ebd1a8904f1ba97aaa103e25e565ea8
Peter Jang <peter.x.jang@gmail.com>
2017-03-14 20:45:23 -0500

    Add recipe friendly created at and prep time model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,17 @@ class Recipe < ApplicationRecord
     # upcased_ingredients
     ingredients.split(", ").map { |ingredient| ingredient.upcase }
   end
+
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours" if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
 end
