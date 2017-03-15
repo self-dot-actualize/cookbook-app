fbaa1eb1e17a92371a5196274616afb35b369ada
Peter Jang <peter.x.jang@gmail.com>
2017-03-14 20:29:27 -0500

    Show recipe ingredients as a list using a model method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,10 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    # upcased_ingredients = []
+    # ingredients.split(", ").each do |ingredient|
+    #   upcased_ingredients << ingredient.upcase
+    # end
+    # upcased_ingredients
+    ingredients.split(", ").map { |ingredient| ingredient.upcase }
+  end
 end
